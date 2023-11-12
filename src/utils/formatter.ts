d3e3c1c271138bef7e446892cd69325818273e10
Temi-pinheiro <temitopepinheiro@gmail.com>
2023-11-12 07:03:28 +0100

    for the love of god

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -2,19 +2,6 @@ import dayjs from 'dayjs';
 import LocalizedFormat from 'dayjs/plugin/localizedFormat';
 
 dayjs.extend(LocalizedFormat);
-let userLocale = 'en-NG';
-if (navigator && 'userAgent' in navigator) {
-  userLocale = navigator.language;
-} else {
-  console.error('Currently dont have access to navigator, fallign back');
-}
-const currencyLocale = Intl.NumberFormat.supportedLocalesOf(userLocale, {
-  style: 'currency',
-  currency: 'NGN',
-});
-const dateFormatter = new Intl.DateTimeFormat(userLocale, {
-  dateStyle: 'short', // You can use 'short', 'medium', 'long', or 'full' based on the desired format
-});
 
 export const getFormattedDate = (date: string | undefined | Date) => {
   if (!date) return '';
